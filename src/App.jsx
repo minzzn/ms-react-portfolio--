b4fdb4b69000f37d9f6ba099d7027509e760bd7f
김민주 <97500865+minzzn@users.noreply.github.com>
2024-09-26 11:01:28 +0000

    commit portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "김민주",
+  title: "프론트엔드 개발자",
+  email: "minzuicnk@naver.com",
+  gitHub: "minzzn",
+  instagram: "@__jokebear__",
+  linkedIn: "null..",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
